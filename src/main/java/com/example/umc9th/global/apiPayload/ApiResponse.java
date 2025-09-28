2841909ee7239a3d8f3a41392437a28bd5881a53
rlawngjs0313 <juheon0313@naver.com>
2025-09-28 21:15:12 +0900

    ✨ feat: 응답 통일 구현

---
+++ src/main/java/com/example/umc9th/global/apiPayload/ApiResponse.java
@@ -0,0 +1,56 @@
+package com.example.umc9th.global.apiPayload;
+
+import com.example.umc9th.global.apiPayload.code.BaseSuccessCode;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private Boolean isSuccess;
+
+    @JsonProperty("code")
+    private String code;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("result")
+    private T result;
+
+    // 200 OK
+    public static <T> ApiResponse<T> ok() {
+        return new ApiResponse<T>(true, HttpStatus.OK.toString(), HttpStatus.OK.getReasonPhrase(), null );
+    }
+
+    // 201 Created
+    public static <T> ApiResponse<T> created(String message) {
+        return new ApiResponse<T>(true, HttpStatus.CREATED.toString(), message, null );
+    }
+
+    // 그 이외 성공 응답(result 포함)
+    public static <T> ApiResponse<T> onSuccess(BaseSuccessCode code, T result) {
+        return new ApiResponse<T>(true, code.getCode()., code.getMessage(), result);
+    }
+
+    // 그 이외 성공 응답(result 미포함)
+    public static <T> ApiResponse<T> onSuccess(BaseSuccessCode code) {
+        return new ApiResponse<T>(true, code.getCode(),  code.getMessage(), null );
+    }
+
+    // 실패 응답(result 포함)
+    public static <T> ApiResponse<T> onFailure(BaseSuccessCode code, T result) {
+        return new ApiResponse<T>(false, code.getCode(), code.getMessage(), result);
+    }
+
+    // 실패 응답(result 미포함)
+    public static <T> ApiResponse<T> onFailure(BaseSuccessCode code) {
+        return new ApiResponse<T>(false, code.getCode(),  code.getMessage(), null );
+    }
+}
