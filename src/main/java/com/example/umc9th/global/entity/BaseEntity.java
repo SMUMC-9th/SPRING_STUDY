9fdeea8d2f41648098b4e2f3f8f6d32e6cd1ca10
rlawngjs0313 <juheon0313@naver.com>
2025-10-05 23:10:32 +0900

    ✨ feat: 엔티티 생성

---
+++ src/main/java/com/example/umc9th/global/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.example.umc9th.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
