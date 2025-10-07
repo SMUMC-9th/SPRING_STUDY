01663c5253ff5e1d380ab0b6644b6fff542ba24a
tuna1025 <jo215098@gmail.com>
2025-10-08 01:29:07 +0900

    ✨feat: article 생성기능, 조회기능 작성

---
+++ src/main/java/com/example/umc9th/domain/article/service/query/ArticleQueryServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.umc9th.domain.article.service.query;
+
+import com.example.umc9th.domain.article.converter.ArticleConverter;
+import com.example.umc9th.domain.article.dto.response.ArticleResponseDTO;
+import com.example.umc9th.domain.article.entity.Article;
+import com.example.umc9th.domain.article.exception.ArticleErrorCode;
+import com.example.umc9th.domain.article.exception.ArticleException;
+import com.example.umc9th.domain.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+// Query는 읽기만 하니 ReadOnly로 작성
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ArticleQueryServiceImpl implements ArticleQueryService {
+
+    private final ArticleRepository articleRepository;
+
+    @Override
+    public List<Article> getArticles() {
+        // 구현, 힌트: findAll()
+        return articleRepository.findAll();
+    }
+
+    @Override
+    public ArticleResponseDTO.ArticleDetailDTO getArticle(Long id) {
+        // 구현, 힌트: findById(Long id)
+        Article article = articleRepository.findById(id)
+                .orElseThrow(() -> new ArticleException(ArticleErrorCode.ARTICLE_NOT_FOUND));
+        // findById의 결과로 Optional 형태가 나올 예정인데 1주차 워크북의 구현된 Error code를 참고하여 ArticleErrorCode를 작성해보시고 직접 에러를 발생시키셔도 좋고 아니면 일단 .get()을 사용하시고 제가 세미나에서 알려드릴게요
+        return ArticleConverter.toDetailDTO(article);
+    }
+}
