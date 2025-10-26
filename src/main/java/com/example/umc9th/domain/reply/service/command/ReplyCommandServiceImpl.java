d93e38a6b288fcdd8fb0b75845d20d3d42ff9d6a
rlawngjs0313 <juheon0313@naver.com>
2025-10-26 18:40:55 +0900

    ✨ feat: 게시글 연동 로직 추가

--- src/main/java/com/example/umc9th/domain/reply/service/command/ReplyCommandServiceImpl.java
+++ src/main/java/com/example/umc9th/domain/reply/service/command/ReplyCommandServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.umc9th.domain.reply.service.command;
 
 
+import com.example.umc9th.domain.article.entity.Article;
+import com.example.umc9th.domain.article.exception.ArticleException;
+import com.example.umc9th.domain.article.exception.code.ArticleErrorCode;
+import com.example.umc9th.domain.article.repository.ArticleRepository;
 import com.example.umc9th.domain.reply.converter.ReplyConverter;
 import com.example.umc9th.domain.reply.dto.req.ReplyRequestDTO;
 import com.example.umc9th.domain.reply.entity.Reply;
@@ -15,19 +19,20 @@ import org.springframework.stereotype.Service;
 public class ReplyCommandServiceImpl implements ReplyCommandService {
 
     private final ReplyRepository replyRepository;
+    private final ArticleRepository articleRepository;
 
-    // 게시글 생성
+    // 댓글 생성
     @Override
     public Reply createReply(
             ReplyRequestDTO.CreateReply dto
     ) {
 
-        // 객체 생성
-        Reply reply = ReplyConverter.toReply(dto);
-
         // 게시글 연동: 게시글 조회
-//        Article article = articleRepository.findById(id)
-//                .orElseThrow(() -> new ArticleException(ArticleErrorCode.NOT_FOUND));
+        Article article = articleRepository.findById(dto.articleId())
+                .orElseThrow(() -> new ArticleException(ArticleErrorCode.NOT_FOUND));
+
+        // 객체 생성
+        Reply reply = ReplyConverter.toReply(dto, article);
 
         // save
         return replyRepository.save(reply);
