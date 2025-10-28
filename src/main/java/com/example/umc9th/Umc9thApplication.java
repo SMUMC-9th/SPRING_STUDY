2b111d51db79c2eb034e50ac86bace4f64fcb655
tnqlsqkr <129366603+tnqlsqkr@users.noreply.github.com>
2025-10-28 15:57:10 +0900

    Revert "✨ [Feat] CRUD 중 CR"

--- src/main/java/com/example/umc9th/Umc9thApplication.java
+++ src/main/java/com/example/umc9th/Umc9thApplication.java
@@ -2,9 +2,7 @@ package com.example.umc9th;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class Umc9thApplication {
 
--- src/main/java/com/example/umc9th/domain/article/dto/request/ArticleReqDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.umc9th.domain.article.dto.request;
-
-public record ArticleReqDTO (
-        String title,
-        String content
-) {
-}
