e3293bf1ffad1f786e20a59532c208ee9be7cf3b
Seojun Lee <leeseojun34@gmail.com>
2025-11-09 19:30:48 +0900

    ✨ feat: CORS 설정

--- src/main/java/com/example/umc9th/global/config/SecurityConfig.java
+++ src/main/java/com/example/umc9th/global/config/SecurityConfig.java
@@ -10,6 +10,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -34,6 +40,8 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             // Http Basic 인증 방식 비활성화
             .httpBasic(AbstractHttpConfigurer::disable)
+            // CORS 적용
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             // formLogin 설정
             .formLogin(formLogin -> formLogin
                 // Form login에서 사용하는 SecurityContextRepository 설정
@@ -52,6 +60,33 @@ public class SecurityConfig {
         ;
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:8080"
+        ));
+
+        configuration.setAllowedMethods(Arrays.asList(
+            "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        configuration.setMaxAge(3600L);
+
+        configuration.setExposedHeaders(Arrays.asList(
+            "Authorization", "Content-Type"
+        ));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
