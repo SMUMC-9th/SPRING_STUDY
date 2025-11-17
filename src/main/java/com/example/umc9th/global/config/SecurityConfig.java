3fd0e50f608596d48117dbc554bdccacd2ad5b5f
Seojun Lee <leeseojun34@gmail.com>
2025-11-18 01:20:18 +0900

    ✨ feat: Security 설정 업데이트

--- src/main/java/com/example/umc9th/global/config/SecurityConfig.java
+++ src/main/java/com/example/umc9th/global/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.example.umc9th.global.config;
 
+import com.example.umc9th.global.auth.CustomAccessDeniedHandler;
+import com.example.umc9th.global.auth.CustomEntryPoint;
+import com.example.umc9th.global.auth.CustomUserDetailsService;
+import com.example.umc9th.global.jwt.JwtFilter;
+import com.example.umc9th.global.jwt.JwtUtil;
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,44 +26,38 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomEntryPoint customEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     // 아래 3개는 Swagger에 대한 URL
     private String[] allowUrl = {
         "/auth/sign-up",
-//        "/swagger-ui/**",
-//        "/swagger-resources/**",
-//        "/v3/api-docs/**",
+        "/auth/login",
+        "/swagger-ui/**",
+        "/swagger-resources/**",
+        "/v3/api-docs/**",
     };
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            // 어떤 URL에 Security를 걸 것인지 permitAll을 허용, hasRole은 특정 role이 있어야 허용, authenticated는 인증 필요
             .authorizeHttpRequests(request -> request
                 .requestMatchers(allowUrl).permitAll()
                 .anyRequest().authenticated()
             )
-            // CSRF 비활성화
             .csrf(AbstractHttpConfigurer::disable)
-            // Http Basic 인증 방식 비활성화
             .httpBasic(AbstractHttpConfigurer::disable)
-            // CORS 적용
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            // formLogin 설정
-            .formLogin(formLogin -> formLogin
-                // Form login에서 사용하는 SecurityContextRepository 설정
-                .securityContextRepository(securityContextRepository())
-                // 로그인 성공 시 URL, 보통은 SuccessfulHandler를 많이 사용하지만 간단하게 보기 위해 이 방식 사용
-                .defaultSuccessUrl("/swagger-ui/index.html")
-            )
-            // 세션 관리 방식 설정, IF_REQUIRED는 필요 시에만 세션을 생성
-            .sessionManagement(sessionManagement -> sessionManagement
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-            )
-            // SecurityContext에서 사용할 SecurityContextRepository 설정
-            .securityContext(context -> context
-                .securityContextRepository(securityContextRepository())
+            .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(exception -> exception
+                .authenticationEntryPoint(customEntryPoint)
+                .accessDeniedHandler(customAccessDeniedHandler)
             )
         ;
 
@@ -90,13 +92,16 @@ public class SecurityConfig {
     }
 
     @Bean
-        // AuthenticationProvider에서 사용할 passwordEncoder 설정
+    public Filter jwtFilter() {
+        return new JwtFilter(jwtUtil, customUserDetailsService);
+    }
+
+    @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-        // SecurityContextRepository 빈 등록
     SecurityContextRepository securityContextRepository() {
         return new HttpSessionSecurityContextRepository();
     }
